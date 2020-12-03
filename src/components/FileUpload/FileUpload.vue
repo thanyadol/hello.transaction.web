90d4a02fc6c8c7b80f99efd7e2fb2b322eedfaae
thanyadol <rockwalnut@gmail.com>
2020-12-03 21:07:36 +0700

    add bootrap package dependency

---
+++ src/components/FileUpload/FileUpload.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="input-group | mb-1 | p-3">
+    <!-- It's better to be explicit -->
+    <!-- Don't forget to bind the property with ":" (we forget all the time!) -->
+      <input class="form-control file" type="file" id="file" ref="file" v-on:change="onChangeFileUpload()"/>
+      <button :disabled="disableSubmit" class="btn btn-primary right" v-on:click="submitForm()">Upload</button>
+
+  </div>
+</template>
+
+<script>
+/// //////////////////////////////////////
+export default {
+  data () {
+    return {
+      fileInfos: []
+    }
+  },
+  // props: [ 'value' ],
+  computed: {
+  },
+  methods: {
+    onChange: function () {
+      console.log("change handle")
+    },
+    submitForm: function () {
+      console.log("submit handle")
+    }
+  },
+  mounted () {
+    console.log("mounted handle")
+  }
+}
+</script>
+<style scoped>
+.mt25 {
+  margin-top: 10px;
+}
+input {
+  border:none;
+  background-color: transparent;
+  color: #495057;
+}
+.form-control:focus {
+  box-shadow: none;
+}
+input:focus {
+    outline: none;
+}
+.grey {
+  color: gray;
+}
+
+.success {
+  color: forestgreen;
+}
+
+.fail {
+  color: crimson;
+}
+
+.block {
+  display: block;
+  width: 100%;
+  padding: 1.1em;
+}
+
+.input-group {
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    padding: 0px 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 
-Vue.config.productionTip = false
+// bootstrap style site
+import './assets/css/app.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
+  components: { App },
+  template: '<App/>'.replace,
+
+  // fa icon render
+  render: h => h(App)
 }).$mount('#app')
+
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
+// require('../node_modules/font-awesome/css/font-awesome.min.css')
+
+// require('./assets/css/layout.css')
+// require('./assets/css/app.css')
